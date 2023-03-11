f9b683584f3c85b468b0bd5491c0fc38ac3d061a
Sophia Cook <sophia.cook@codecentric.de>
2023-03-11 20:23:09 +0100

    use correct names

--- src/space-ship-store-front/api-adapter/getSpaceShips.ts
+++ src/space-ship-store-front/api-adapter/getSpaceShips.ts
@@ -1,6 +1,6 @@
 import {getInYard} from './getInYard';
 import {getCatalogue} from './getCatalogue';
-import {mapToDomainSpaceShips} from './mapToShips';
+import {mapToDomainSpaceShips} from './mapToSpaceShips';
 
 export const getSpaceShips = async ({getInYard_ = getInYard, getCatalogue_ = getCatalogue} = {getInYard_ : getInYard, getCatalogue_ : getCatalogue})=>{
     const [inYard, catalogue ] = await Promise.all([getInYard_(), getCatalogue_()])
