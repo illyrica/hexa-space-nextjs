b3dc7304240369ae77fb1a1c8be846b5977c20d3
Marco Emrich <marco.emrich@codecentric.de>
2023-03-12 18:27:21 +0100

    build db-adapter for claps

--- src/space-ship-store-front/db-adapter/clap-store.spec.ts
+++ src/space-ship-store-front/db-adapter/clap-store.spec.ts
@@ -1,15 +1,23 @@
 import { describe } from "vitest";
-import { getClaps } from "./claps-store";
+import { incClapsForId, getClaps } from "./claps-store";
 
 describe("getClaps (integration test)", () => {
-  it("should read claps from json", () => {
-    const result = getClaps();
+  it("should read claps from json", async () => {
+    const result = await getClaps();
 
-    expect(result.length).toBeGreaterThan(1);
+    expect(Object.entries(result).length).toBeGreaterThan(0);
+    expect(result["1234"]).toMatchObject(expect.any(Number));
+  });
+});
 
-    expect(result[0]).toMatchObject({
-      shipId: expect.any(Number),
-      claps: expect.any(Number),
-    });
+describe("incClapsForSpaceShip", () => {
+  it("should write the increased by 1 number to file (integration test)", async () => {
+    const numberOfClapsBefore = (await getClaps())["1234"];
+
+    await incClapsForId("1234");
+
+    const numberOfClapsAfter = (await getClaps())["1234"];
+
+    expect(numberOfClapsAfter).toEqual(numberOfClapsBefore + 1);
   });
 });
--- src/space-ship-store-front/db-adapter/claps-store.ts
+++ src/space-ship-store-front/db-adapter/claps-store.ts
@@ -1,3 +1,13 @@
-import claps from "./clap-store.json";
+import { Config, JsonDB } from "node-json-db";
 
-export const getClaps = () => claps;
+const STORE_FILE = "clap-store.json";
+
+const db = new JsonDB(new Config(STORE_FILE, true, true));
+
+export const getClaps = async (): Promise<Record<string, number>> =>
+  await db.getData("/");
+
+export const incClapsForId = async (id: string) => {
+  const value = (await getClaps())[id];
+  await db.push(`/${id}`, value + 1, false);
+};
