9044c73ab8af45d5959b7e7454eac287f38bd522
Anna Maier <anna.maier@codecentric.de>
2024-02-23 09:16:58 +0100

    Make details page less accessible

--- src/app/shipdetails/[shipId]/page.tsx
+++ src/app/shipdetails/[shipId]/page.tsx
@@ -1,5 +1,9 @@
 import {getSpaceShip} from "@/bounded_contexts/space-ship-store-front/api-adapter/getSpaceShip";
-import ShipDetails from "@/app/shipdetails/[shipId]/ship-details";
+import {ShipDetailsComponent} from "@/bounded_contexts/space-ship-store-front/ui-adapter/ship-details-component";
+import {getClaps} from "@/bounded_contexts/space-ship-store-front/api-adapter/getClaps";
+import {
+  getClapsForShip
+} from "@/bounded_contexts/space-ship-store-front/clap-adapter/attachClapsToShips";
 
 export default async function ShipDetailsPage({
   params: {shipId},
@@ -7,9 +11,11 @@ export default async function ShipDetailsPage({
   params: { shipId: string };
 }) {
   const ship = await getSpaceShip(shipId)
+  const claps = await getClaps();
+  const clapsForShip = ship ? getClapsForShip({claps, ship}) : 0;
   return ship && (
-    <main>
-        <ShipDetails ship={ship} />
-    </main>
+    <div>
+      <ShipDetailsComponent key={ship.id} ship={ship} claps={clapsForShip}/>
+    </div>
   );
 }
