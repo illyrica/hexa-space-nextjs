81ae08aa406e8bce637bc117e229d1cd3485a55d
Sophia Cook <sophia.cook@codecentric.de>
2025-03-26 15:02:06 +0100

    fix: missing key warning in weapon component

--- src/app/weapons/page.tsx
+++ src/app/weapons/page.tsx
@@ -4,14 +4,14 @@ import styles from '../page.module.css';
 
 
 export default async function Ships() {
-  const weaponInventory = await getWeaponInventory()
+    const weaponInventory = await getWeaponInventory()
 
-  return (
-    <main className={styles.main}>
-      <h1>Weapon Inventory</h1>
-      <div className='weapon-inventory'>
-        {weaponInventory.map(w => <WeaponComponent weapon={w}></WeaponComponent>)}
-      </div>
-    </main>
-  )
+    return (
+        <main className={styles.main}>
+            <h1>Weapon Inventory</h1>
+            <div className='weapon-inventory'>
+                {weaponInventory.map(w => <WeaponComponent key={w.name} weapon={w}></WeaponComponent>)}
+            </div>
+        </main>
+    )
 }
